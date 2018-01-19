bf6550e86ebfce8c0590e6f848601cb46a614a8b
Strahinja-DESKTOP <strahinja.gligovic@gmail.com>
2018-01-19 21:43:24 +0100

    movie comments

--- backend/db/models/movie.model.js
+++ backend/db/models/movie.model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
+// hteli bismo da prikažemo više informacija o jednom filmu
+// u tu svrhu ćemo koristiti javno dostupan tmdb API
 var movieSchema = new mongoose.Schema({
     title: String,
     releaseDate: String,
     director: String,
+    // ovo polje služi za asocijaciju filma sa API
     tmdb: Number
 });
 
