3c6f5c470963f37a55f794750d66f454af3454dd
Strahinja-DESKTOP <strahinja.gligovic@gmail.com>
2018-02-27 23:31:43 +0100

    address quickfix

--- frontend/src/app/util/address/address.component.ts
+++ frontend/src/app/util/address/address.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Address } from '../../models/address.model';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { countries } from '../const';
 
 @Component({
   selector: 'app-address',
@@ -10,6 +11,7 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 export class AddressComponent implements OnInit {
   // UI
   disableInputs = true;
+  countries = countries;
 
   @Input() address: Address;
 
